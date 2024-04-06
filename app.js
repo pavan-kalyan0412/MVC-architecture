3662ad19db5d6a47644ea7ffc30406e32359f0ec
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-04-06 20:53:39 +0530

    added routes, app.js

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const express = require('express');
+const { Mongoose } = require('mongoose');
 const app = express();
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended:true}));
+app.use(express.json());
+
+Mongoose.connect("mongodb://localhost:27017/MVC", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error(err));
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/', authRoutes);
+
+
 const PORT = process.env.PORT || 4111;
 app.listen(PORT, console.log("Server has started at port: " + PORT))
