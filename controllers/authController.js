bc81e3148b2375aa7fb092e6bb96fdc20875f191
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-04-06 22:45:23 +0530

    added JWT concept

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,41 +1,34 @@
+// controllers/authController.js
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-exports.register = async (req,res) =>{
+exports.register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
-         const existingUser = await User.findOne({ email });
-          if (existingUser) {
-            return res.status(400).json({error: 'Email already exist'})
-          }
-         await new User ({ username, email, password:hashedPassword }).save();
-          res.status(201).json({ message: 'user registered successfully'})
-    } catch (error) {
-        res.status(500).json({error: error.message})
-        
-    }
-}
-
-exports.login = async(req,res) =>{
-    try {
-         const { email, password } = req.body;
-
-         const user = await User.findOne( { email });
-
-         if (!user){
-            res.status(401).json({ error: 'user not found'})
-         }
-
-         const isPasswordMatch = await bcrypt.compare(password, user.password);
-
-         if(!isPasswordMatch){
-            res.status(401).json({error:'invalid password'})
-         }else{
-            res.status(200).json({message: 'you are welcome'})
-         }
+        const user = new User({ username, email, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-}
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            throw new Error('Invalid password');
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(401).json({ error: error.message });
+    }
+};
